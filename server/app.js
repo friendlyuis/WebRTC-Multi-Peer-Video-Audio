474ead46d74521dc76eba056c1f8525ce9158bf7
friendlyuis <tommy@luong.co>
2020-07-08 19:56:31 -0400

    working

--- server/app.js
+++ server/app.js
@@ -1,26 +1,86 @@
 var express = require('express')
-  , routes = require('./routes');
 
-var app = module.exports = express.createServer();
+var app = (module.exports = express.createServer())
 
-var io = require('socket.io')(app);
+var io = require('socket.io')(app)
 
-io.on('connection', function(socket){
-	io.sockets.emit("user-joined", socket.id, io.engine.clientsCount, Object.keys(io.sockets.clients().sockets));
+io.on('connection', function (socket, podId) {
+  console.log('connection')
 
-	socket.on('signal', (toId, message) => {
-		io.to(toId).emit('signal', socket.id, message);
-  	});
+  io.sockets.emit(
+    'user-joined',
+    socket.id,
+    io.engine.clientsCount,
+    Object.keys(io.sockets.clients().sockets)
+  )
 
-    socket.on("message", function(data){
-		io.sockets.emit("broadcast-message", socket.id, data);
+  socket.on('signal', (toId, message) => {
+    console.log('signal')
+    io.to(toId).emit('signal', socket.id, message)
+  })
+
+  socket.on('join', (data) => {
+    socket.join(data.podId, (err) => {
+      console.log('is it sending this?', {data})
+      if (err) {
+        console.log({ err })
+      }
+      io.to(data.podId).emit(
+        'message',
+        socket.id,
+        JSON.stringify({
+          joinUserId: data.joinUserId,
+          offerOrigin: socket.id,
+          podId: data.podId,
+          offer: data.offer,
+          answer: data.answer,
+        })
+      )
     })
+  })
 
-	socket.on('disconnect', function() {
-		io.sockets.emit("user-left", socket.id);
-	})
-});
+  socket.on('leave', (podId) => {
+    socket.leave(podId, (err) => {
+      if (err) {
+        console.log({ err })
+      }
+    })
+  })
 
-app.listen(3000, function(){
-	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+  socket.on('message', (message) => {
+    try {
+      const data = JSON.parse(message)
+      if(data.offer) {
+        console.log('got offer', {data})
+      }
+
+      io.to(data.to ? data.to : data.podId).emit(
+        'message',
+        socket.id,
+        JSON.stringify({
+          to: data.to,
+          joinUserId: data.joinUserId,
+          podId: data.podId,
+          offerOrigin: data.offerOrigin,
+          answer: data.answer,
+          iceCandidate: data.iceCandidate,
+          offer: data.offer,
+        })
+      )
+    } catch (err) {
+      console.log({ err })
+    }
+  })
+
+  socket.on('disconnect', function () {
+    io.sockets.emit('user-left', socket.id)
+  })
+})
+
+app.listen(3001, function () {
+  console.log(
+    'Express server listening on port %d in %s mode',
+    app.address().port,
+    app.settings.env
+  )
+})
